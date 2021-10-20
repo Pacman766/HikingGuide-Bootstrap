2e1b806fb12c7b8f91c5576ca44e1d7ee6d34a12
pacman766 <3334976@gmail.com>
2021-10-20 11:07:46 +0300

    2010 1107

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function readMore() {
+    let dots = document.getElementById("dots");
+    let readMore = document.getElementById("read_more");
+    let readMoreBtn = document.getElementById("read_more_btn");
+    let readMoreArrow = document.getElementById("read_more_arrow");
+
+    if()
+}
