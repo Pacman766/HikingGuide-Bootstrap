d24da07cbcd625ac2f03c6379eaa02985a61a6fb
unknown <3334976@gmail.com>
2021-10-20 14:14:27 +0300

    2010 1414

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 function readMore() {
-    let dots = document.getElementById("dots");
-    let readMore = document.getElementById("read_more");
-    let readMoreBtn = document.getElementById("read_more_btn");
-    let readMoreArrow = document.getElementById("read_more_arrow");
+  let dots = document.getElementById("dots");
+  let readMore = document.getElementById("read_more");
+  let readMoreBtn = document.getElementById("read_more_btn");
 
-    if()
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    readMore.style.display = "none";
+    readMoreBtn.innerHTML = "read more";
+  } else {
+    dots.style.display = "none";
+    readMore.style.display = "inline";
+    readMoreBtn.innerHTML = "less";
+  }
 }
+
+const offset = 100;
+const scrollUp = document.querySelector('.scroll-up')
