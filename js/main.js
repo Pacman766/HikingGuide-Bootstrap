b4665ce314a98f849f5b94576271d0d44d69af4a
pacman766 <3334976@gmail.com>
2021-11-05 20:16:23 +0300

    0511 2016

--- js/main.js
+++ js/main.js
@@ -98,4 +98,43 @@ document.querySelector(".page-link-03").addEventListener("click", function () {
 
 // Табы
 
+const tabs = document.querySelectorAll('.tabheader__item'), //  табы
+      tabsContent = document.querySelectorAll('.tabcontent'), // картинки
+      tabParent = document.querySelector('.tabheader__items'); // родитель табов
 
+// ф-ция для скрытия табов
+function hideTabContent() {
+  tabsContent.forEach(item => {
+    item.classList.add('hide');
+    item.classList.remove('show', 'fade');
+
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  });
+}
+
+// ф-ция для показа 1го эл-та табов
+function showTabContent(i = 0) {
+  tabsContent[i].classList.add('show', 'fade');
+  tabsContent[i].classList.remove('hide');
+  tabs[i].classList.add('tabheader__item_active');
+    
+}
+
+hideTabContent();
+showTabContent();
+
+// обработич события с помощью делегирования
+tabParent.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if(target && target.classList.contains('tabheader__item')) {
+    tabs.forEach((elem, i) => {
+      if(target == elem) {
+        hideTabContent();
+        showTabContent(i);
+      }
+    });
+  }
+});
